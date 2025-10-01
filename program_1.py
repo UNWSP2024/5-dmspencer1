ca2de02decd915da72a01d25ffeec3c885b610f5
Dalila Spencer <dmspencer@students.unwsp.edu>
2025-10-01 13:40:31 -0500

    Week 5 programs complete

--- program_1.py
+++ program_1.py
@@ -1,3 +1,7 @@
+# Title: Kilometer Converter
+# Author: Dalila Spencer
+# Date: 2025-09-30
+
 # Program #1: Kilometer Converter
 # Write a program that asks the user to enter a distance in kilometers, 
 # then converts that distance to miles.  The conversion formula is as follows:  
@@ -7,9 +11,12 @@
 
 def kilometer_conversion(kilometers):    
     miles = 0.0
+
     ######################
-    # WRITE YOUR CODE HERE
-    ######################    
+
+    miles = kilometers * 0.6214
+
+    ######################
 
 
     # Return the variable to the calling function
@@ -21,6 +28,11 @@ def kilometer_conversion(kilometers):
 if __name__ == '__main__':
     # Get User Input
     print('in main')
+
+    kilometers = float(input('Enter a distance in kilometers to convert it to miles: '))
+
     # Call kilometer_conversion, don't forget to pass in the kilometer parameter!
-    
+    miles = kilometer_conversion(kilometers)
+
     # Display the miles
+    print(kilometers, 'kilometers = ', miles, 'miles')
